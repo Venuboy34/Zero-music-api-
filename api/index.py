1335edfaa5b259055207c6c6e0f8fbc9c20b9296
Venuboy34 <venura505@gmail.com>
2025-04-13 10:45:52 +0530

    Update and rename index.py to api/index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,6 @@ import json
 from urllib.parse import parse_qs
 
 def handler(request):
-    # Get the 'name' from the query params
     params = parse_qs(request.query_string.decode())
     name = params.get('name', [None])[0]
 
@@ -17,9 +16,8 @@ def handler(request):
             }, indent=2)
         }
 
-    # Use yt-dlp to search and get music details
-    search_command = f'yt-dlp --default-search ytsearch1:"{name}" --get-title --get-id --get-thumbnail'
-    process = subprocess.Popen(search_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    command = f'yt-dlp --default-search ytsearch1:"{name}" --get-title --get-id --get-thumbnail'
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
 
     if process.returncode != 0:
@@ -32,22 +30,18 @@ def handler(request):
             }, indent=2)
         }
 
-    # Get the music data from stdout
     output = stdout.decode().strip().split("\n")
     if len(output) < 3:
         return {
             "statusCode": 500,
             "body": json.dumps({
                 "status": "error",
-                "message": "Could not find any result"
+                "message": "No valid results"
             }, indent=2)
         }
 
     title, video_id, thumbnail = output
 
-    mp3_url = f'https://api.vevioz.com/@api/button/mp3/{video_id}'
-
-    # Return only the details and download link
     return {
         "statusCode": 200,
         "body": json.dumps({
@@ -55,7 +49,7 @@ def handler(request):
             "result": {
                 "title": title,
                 "thumbnail": thumbnail,
-                "mp3": f'https://dl.musiczero.vercel.app/mp3/{video_id}.mp3'
+                "mp3": f"https://dl.musiczero.vercel.app/mp3/{video_id}.mp3"
             }
         }, indent=2)
     }
